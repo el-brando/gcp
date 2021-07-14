5d7ed8b293b5f52168f7354551c6795dc25fc4df
Brandon Harroff <brandoh@gmail.com>
2021-07-14 14:11:47 -0500

    updated instance namme

--- us-west1/config/compute.tf
+++ us-west1/config/compute.tf
@@ -1,7 +1,7 @@
 module "compute_test" {
   source = "../../modules/base/compute/gce_instance"
 
-  base_instance_name = "test_instance"
+  base_instance_name = "test"
   mcount = "1"
   tags = ["test"]
 }
