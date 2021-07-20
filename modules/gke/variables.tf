38b218fcb5063b9c0457ce2677565260e9c56e12
Josse <jossec10@gmail.com>
2021-07-20 15:07:14 -0400

    refactored gke module files to create a separate node pool

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -12,3 +12,22 @@ variable "initial_node_count"{
     description = "number of nodes in the default node pool"
     default = 1
 }
+
+variable "remove_default_node_pool"{
+    description = "Are you using a separate node pool"
+    default = false
+}
+
+variable "node_pool_name" {
+    description = "Name your node pool"
+}
+
+variable "machine_type"{
+    description = "what machine should be used for the nodes in the pool"
+    default = "e2-micro"
+}
+
+variable "node_count"{
+    description = "how many nodes per instance group in your separate node pool"
+    default = 1
+}
