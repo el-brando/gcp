6d92e160bb9c86dab14412d968a0345387159a11
Josse <jossec10@gmail.com>
2021-07-20 16:09:57 -0400

    reupdated the gke.tf file to create a cluster with separate node pool

--- us-west1/config/gke.tf
+++ us-west1/config/gke.tf
@@ -3,7 +3,9 @@
 module "gke_cluster_test"{
     source = "../../modules/gke"
 
-    name = "test-cluster"
-    location = "us-west1"
-    initial_node_count = 1
+    name            = "test-cluster"
+    location        = "us-west1"
+    
+    node_pool_name  = "my-test-node-pool"
+    machine_type    = "e2-small"
 }
