f00e6a7b70150325c3844492cba78d613b05ac97
Brandon Harroff <brandoh@gmail.com>
2021-07-21 13:05:49 -0500

    more troubbleshooting

--- us-west1/config/bhgke.tf
+++ us-west1/config/bhgke.tf
@@ -16,7 +16,7 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "bhcluster_nodepool"
-  cluster    = "bhcluster"
+  cluster    = google_container_cluster.primary.name
   node_count = 1
 
   node_config {
