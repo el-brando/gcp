86d5d3c95f5a99118d1d9b3a02683b39caf9523e
Josse <jossec10@gmail.com>
2021-07-27 17:22:52 -0400

    removed redundant module from /gke_autopilot main.tf

--- modules/gke_autopilot/main.tf
+++ modules/gke_autopilot/main.tf
@@ -11,16 +11,5 @@ resource "google_container_cluster" "autopilot"{
     enable_autopilot            = var.enable_autopilot  
 }
 
-module "create_autopilot_cluster"{
-    source                      = "./gke_autopilot"
-    
-    account_id                  = var.account_id
-    display_name                = var.display_name
-
-    cluster_name                = var.cluster_name
-    location                    = var.location
-    enable_autopilot            = var.enable_autopilot 
-}
-
 # GKE provisions and managaes node pools, 
 # User cannot configure node pool settings in autopilot mode
