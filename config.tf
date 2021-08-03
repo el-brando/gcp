b6d0df437e42bff7d56d0c69c77e825116b247dd
Josse Carballo <jossec10@gmail.com>
2021-08-03 16:43:44 -0400

    adding provider to config.tf file to fix issue

--- config.tf
+++ config.tf
@@ -14,3 +14,11 @@ provider "google" {
   region  = "us-west1"
   #zone    = "us-west1-a"
 }
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
